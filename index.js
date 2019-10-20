38273885027bb47c3fd9cf9578cfbfccc1e3b525
Irina Kagan <irinagamb@gmail.com>
2019-10-20 20:20:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function findMatching(drivers, name){
    if (match = drivers.filter(drivers => drivers.toUpperCase() === name.toUpperCase())){
        return 'name';
    }
-  else if () {
+  else {
     return [ ];
   }
   };
