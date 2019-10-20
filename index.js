99d78dfe21052f53a44d7b5672d26cd800f25ef6
Irina Kagan <irinagamb@gmail.com>
2019-10-20 20:45:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function findMatching(drivers, name){
     const match = drivers.filter(drivers => drivers.toUpperCase() === name.toUpperCase())
 
     if(match === name){
-
+      return match  
     }
 
     return [];
