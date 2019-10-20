fb1ae278605d597fa8dc7ddfaf34fc38806ce54b
Irina Kagan <irinagamb@gmail.com>
2019-10-20 21:14:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function findMatching(drivers, name){
   };
 
 function fuzzyMatch(drivers, letters){
-  const match = drivers.filter(drivers => drivers.substring(0,1) === letters)
+  const match = drivers.filter(drivers => drivers.substring(0,2) === letters)
 
   if(match !== undefined){
     return match;
