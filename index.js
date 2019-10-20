ebb30be208ed5e07643be5322e0a449d844a7c70
Irina Kagan <irinagamb@gmail.com>
2019-10-20 20:45:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 // Code your solution here
 function findMatching(drivers, name){
     const match = drivers.filter(drivers => drivers.toUpperCase() === name.toUpperCase())
-      return [];
 
+    if(match === name){
+
+    }
+
+    return [];
   };
 
 
