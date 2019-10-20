f2c9294a28e9bd6ae8f6aa97f1f8fd78dc6c7d07
Irina Kagan <irinagamb@gmail.com>
2019-10-20 20:46:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Code your solution here
 function findMatching(drivers, name){
     const match = drivers.filter(drivers => drivers.toUpperCase() === name.toUpperCase())
-
+  conloge.log(drivers)
     if(match === name){
-      return match  
+      return match
     }
 
     return [];
